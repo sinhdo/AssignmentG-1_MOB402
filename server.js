0e4bae41dd611c20e1fa7b28690fc94ffca820f5
This MC <sinhdtph27866@fpt.edu,vn>
2023-03-24 10:44:01 +0700

    SuaASS

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const expressHbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const User = require("./user.js");
+const Products = require("./product");
+// const fileType = require("file-type ");
+var fs = require("fs");
+const app = express();
+const user = new Array();
+const products = new Array();
+const port = 3000;
 
-
-const expressHbs = require('express-handlebars');
+products.push(
+    new Products(
+      "1",
+      "áo 1",
+      "149000",
+     "https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b"
+    )
+  );
+  products.push(
+    new Products(
+      "2",
+      "áo2",
+      "350000",
+      "https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b"
+    )
+  );
+  products.push(
+    new Products(
+      "3",
+      "áo3",
+      "260000",
+      "https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b"
+    )
+  );
+  products.push(
+    new Products(
+      "4",
+      "áo4",
+      "100000",
+      "https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b"
+    )
+  );
+  products.push(
+    new Products(
+      "5",
+      "áo5",
+      "300000",
+      "https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b")
+  );
 
 app.engine('.hbs', expressHbs.engine({
     extname: "hbs",
@@ -47,10 +93,40 @@ app.get('/trangchu', (req, res) => {
        
        
     }else{
-        res.send("Sai tài khoản hoặc mật khẩu")
+        // res.send("Sai tài khoản hoặc mật khẩu")
+        res.render('defaultView', {
+          layout: 'trangchu',
+      })
     }
    
 });
+app.post("/trangchu/add-user", (req, res) => {
+    const email = req.body.email;
+    const id = req.body.id;
+    const password = req.body.password;
+    const img = req.body.img;
+    if (email == "" || id == "" || password == "" || img == "") {
+      res.status(400).json({ error: "Add user failed" });
+    } else {
+      user.push(new User(id, email, password, img));
+    }
+    res.redirect("/trangchu");
+  });
+  app.post("/products/add-products", (req, res) => {
+    const name = req.body.name;
+    const id = req.body.id;
+    const price = req.body.price;
+    const img = req.body.img;
+    if (name == "" || id == "" || price == "" || img == "") {
+      res.status(400).json({ error: "Add products failed" });
+    } else {
+      products.push(new Products(id, name, price, img));
+    }
+    res.redirect("/products");
+  });
+  app.get("/products", (req, res, next) => {
+    res.render("defaultView", { layout: "layoutProducts", products: products });
+  });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
